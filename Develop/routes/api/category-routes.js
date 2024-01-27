9a4fa400b24f21799eb3416df2a28781f28b1eda
benrodriguezmoran <ben.rodriguezmoran@gmail.com>
2024-01-27 15:01:27 -0500

    delete category

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -57,8 +57,18 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const deletedCategory = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.json(deletedCategory);
+  }
+  catch (error) {
+    handleErrors(res, error);
+  }
 });
 
 module.exports = router;
